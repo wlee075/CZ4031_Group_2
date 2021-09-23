1bdfffac43fec557206691a0cd5237c7009a00ce
wlee075 <wymanleejun@gmail.com>
2021-09-23 11:03:56 +0800

    skeleton helper methods for insert

---
+++ CZ4031_Project1/Controllers/BuildTreeHelperMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CZ4031_Project1.Controllers
+{
+    class BuildTreeHelperMethods
+    {
+        public void search(int key) { }
+        public void insert(int key) { }
+
+    }
+}
